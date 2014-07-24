f13308348dda21086cad3d26c3009a3bc6b5e51e
Taras Lehinevych <tleginevych@gmail.com>
2014-07-24 11:09:08 +0300

    Fixed bug in matrix comparision

--- comparematrix.py
+++ comparematrix.py
@@ -1,89 +1,28 @@
 import cPickle
 import os, sys , numpy
+import helpfunction
 
 modellsi = "movielsi"
 folderlsi= "lsi/"
 
+def printInfo(recall,precision):
+  recall = numpy.array(recall)
+  precision = numpy.array(precision)
+  print "General recall :"
+  print recall
+  print "General precision :"
+  print precision
+  print "Mean of recall :"
+  print numpy.mean(recall)
+  print "Mean of precision"
+  print numpy.mean(precision)
 
-def lenTextInFiles(path):
-  filesLength ={}
-  for root, dirs, files in os.walk(path):
-    files.sort()
-    for fileread in files:
-      if fileread.endswith(".txt"):
-        with open(path + fileread) as a_file:
-          num_lines = sum(1 for line in a_file)
-          filesLength[fileread]=num_lines
-          #print filesLength[a_file]=num_lines
-  return filesLength
-
-def transforMatrix(fileNameText,fileNameGrund,grundMatrix):
-  newGrundTrustMatrix = []
-  saveIndex = []
-  if (len(fileNameText) != len(fileNameGrund)):
-    for i in range (len(fileNameText)):
-      cuttedFileName = os.path.basename(fileNameText[i]).split('.',1)[0]
-      for j in range(0,len(fileNameGrund)):
-        cuttedFileNameGTM =(fileNameGrund[j].split('/'))[4].split('.',1)[0]
-        if (cuttedFileName == cuttedFileNameGTM):
-          saveIndex.append(j)
-    for k in range(0,len(saveIndex)):
-      row = grundMatrix[saveIndex[k]]
-      newrow = []
-      for l in range(0,len(saveIndex)):
-        newrow.append(row[saveIndex[l]])
-      newGrundTrustMatrix.append(newrow)
-
-    return numpy.array(newGrundTrustMatrix)
-  else:
-    return grundMatrix
-
-def readFiles(foldername):
-  fileNames = cPickle.load(open(folderName+folderlsi + modellsi + '.filenames', 'rb'))
-  similarityMatrix = numpy.load(folderName + folderlsi+modellsi+'.npy')
-  grundTrustMatrix = numpy.load(folderName + "gtm/AverageAnnotation.npy")
-  filesGTM = numpy.load(folderName + "gtm/SceneNames.npy")
-  return fileNames,grundTrustMatrix,similarityMatrix,filesGTM
-
-def comparematrix(folderName,coefficient =1.2):
-  fileNames,grundTrustMatrix,similarityMatrix,filesGTM = readFiles(folderName)
-  recall = []
-  precision = []
-  for i in range(0,len(fileNames)):
-    gtm=evaluateGTM(grundTrustMatrix[i],coefficient)
-    sm=evaluateSM(similarityMatrix[i],coefficient)
-    similarity = numpy.intersect1d(gtm,sm)
-    if len(gtm)!= 0 :
-      recall.append(float(len(similarity))/float(len(gtm)))
-    else :
-      recall.append(0)
-    if len(sm)!= 0 :
-      precision.append(float(len(similarity))/float(len(sm)))
-    else :
-      precision.append(0)
-    #debugprint(gtm,sm,similarity,lenTextInFiles(folderName), recall[i],precision[i],fileNames[i],similarityMatrix[i],grundTrustMatrix[i],filesGTM[i])
-  print "Compare Matrix (the most similar values)"
-  printInfo (recall,precision)
-  """
-    #lenTextinFiles = lenTextInFiles(folderName)
-    #newgrundTrustMatrix = transforMatrix(fileNames,filesGTM,grundTrustMatrix)
-
-    if len(gtm)!= 0 and len(sm) !=0 and len(similarity) !=0 :
-      recall.append(float(len(similarity))/float(len(gtm)))
-      precision.append(float(len(similarity))/float(len(sm)))
-      print "Recall "
-      print (float(len(similarity))/float(len(gtm)))
-      print "Precision "
-      print (float(len(similarity))/float(len(sm)))
-      print "--"*30
-  """
-
-def debugprint (gtm,sm,similarity,fileLength,recall,precision,fileNames,similarityLSI,grundTrustMatrix,fileGMT):
+def debugprint (gtm,sm,similarity,fileLength,recall,precision,fileNames,similarityMatrix,grundTrustMatrix,fileGMT):
   print " GTM row : "
   print grundTrustMatrix
   print "--"*30
   print "SM row: "
-  print similarityLSI
+  print similarityMatrix
   print "--"*30
   print " GTM : "
   print gtm
@@ -108,6 +47,33 @@ def debugprint (gtm,sm,similarity,fileLength,recall,precision,fileNames,similari
   print "FileGTMName :"
   print fileGMT
   print "/"*30
+
+def readFiles(foldername):
+  fileNames = cPickle.load(open(folderName+folderlsi + modellsi + '.filenames', 'rb'))
+  similarityMatrix = numpy.load(folderName + folderlsi+modellsi+'.npy')
+  grundTrustMatrix = numpy.load(folderName + "gtm/AverageAnnotation.npy")
+  filesGTM = numpy.load(folderName + "gtm/SceneNames.npy")
+  return fileNames,similarityMatrix,filesGTM,grundTrustMatrix
+
+def comparematrix(folderName,coefficient =1.2):
+  fileNames,similarityMatrix,filesGTM,grundTrustMatrix = readFiles(folderName)
+  recall = []
+  precision = []
+  for i in range(0,len(fileNames)):
+    gtm=evaluateGTM(grundTrustMatrix[i],coefficient)
+    sm=evaluateSM(similarityMatrix[i],coefficient)
+    similarity = numpy.intersect1d(gtm,sm)
+    if len(gtm)!= 0 :
+      recall.append(float(len(similarity))/float(len(gtm)))
+    else :
+      recall.append(0)
+    if len(sm)!= 0 :
+      precision.append(float(len(similarity))/float(len(sm)))
+    else :
+      precision.append(0)
+    #debugprint(gtm,sm,similarity,lenTextInFiles(folderName), recall[i],precision[i],fileNames[i],similarityMatrix[i],grundTrustMatrix[i],filesGTM[i])
+  print "Compare Matrix (the most similar values)"
+  printInfo (recall,precision)
 
 def evaluateGTM(row, coef):
   T_value = 0
@@ -143,7 +109,7 @@ def evaluateSM(row,coef):
 def evaluateSMRandom(row,coef,length):
   random_index = numpy.random.permutation(len(row))
   #TODO: choose the number of return values
-  T_value = 5 #length
+  T_value = 7 #length
   result =[]
   for i in range(0,int(T_value)):
     result.append(random_index[i])
@@ -171,7 +137,7 @@ def evaluateMetric30per(row1, row2):
   return (float(len(similarity))/3)
 
 def comparematrixRandom30percent(folderName,coefficient =1.2):
-  fileNames,grundTrustMatrix,similarityMatrix,filesGTM = readFiles(folderName)
+  fileNames,similarityMatrix,filesGTM,grundTrustMatrix = readFiles(folderName)
   result = []
   for i in range(0,len(fileNames)):
     result.append(evaluateMetric30per(grundTrustMatrix[i],similarityMatrix[i]))
@@ -180,20 +146,8 @@ def comparematrixRandom30percent(folderName,coefficient =1.2):
   print "Mean of result"
   print numpy.mean(numpy.array(result))
 
-def printInfo(recall,precision):
-  recall = numpy.array(recall)
-  precision = numpy.array(precision)
-  print "General recall :"
-  print recall
-  print "General precision :"
-  print precision
-  print "Mean of recall :"
-  print numpy.mean(recall)
-  print "Mean of precision"
-  print numpy.mean(precision)
-
 def comparematrixRandom(folderName,coefficient =1.2):
-  fileNames,grundTrustMatrix,similarityMatrix,filesGTM = readFiles(folderName)
+  fileNames,similarityMatrix,filesGTM,grundTrustMatrix = readFiles(folderName)
   recall = []
   precision = []
   for i in range(0,len(fileNames)):
@@ -208,7 +162,7 @@ def comparematrixRandom(folderName,coefficient =1.2):
       precision.append(float(len(similarity))/float(len(sm)))
     else :
       precision.append(0)
-    #debugprint(gtm,sm,similarity,lenTextInFiles(folderName), recall[i],precision[i],fileNames[i],similarityMatrix[i],grundTrustMatrix[i],filesGTM[i])
+    #debugprint(gtm,sm,similarity,helpfunction.lenTextInFiles(folderName), recall[i],precision[i],fileNames[i],similarityMatrix[i],grundTrustMatrix[i],filesGTM[i])
   print "Compare Matrix (random values from similarity text matrix)"
   printInfo (recall,precision)
 
@@ -229,3 +183,18 @@ if __name__ == '__main__':
   if sys.argv[1] == "-compareMatrix30percent":
     folderName = sys.argv[2]
     comparematrixRandom30percent(folderName)
+
+
+  """
+    #lenTextinFiles = lenTextInFiles(folderName)
+    #newgrundTrustMatrix = transforMatrix(fileNames,filesGTM,grundTrustMatrix)
+
+    if len(gtm)!= 0 and len(sm) !=0 and len(similarity) !=0 :
+      recall.append(float(len(similarity))/float(len(gtm)))
+      precision.append(float(len(similarity))/float(len(sm)))
+      print "Recall "
+      print (float(len(similarity))/float(len(gtm)))
+      print "Precision "
+      print (float(len(similarity))/float(len(sm)))
+      print "--"*30
+  """
--- helpfunction.py
+++ helpfunction.py
@@ -1,5 +1,7 @@
 import os
+import numpy
 from os.path import basename
+
 
 def ensure_dir(f):
   d = os.path.dirname(f)
@@ -11,3 +13,37 @@ def fileNamesCutter(filesNames):
   for x in filesNames:
     files.append(basename(x))
   return files
+
+def lenTextInFiles(path):
+  filesLength ={}
+  for root, dirs, files in os.walk(path):
+    files.sort()
+    for fileread in files:
+      if fileread.endswith(".txt"):
+        with open(path + fileread) as a_file:
+          num_lines = sum(1 for line in a_file)
+          filesLength[fileread]=num_lines
+          #print filesLength[a_file]=num_lines
+  return filesLength
+
+
+def transforMatrix(fileNameText,fileNameGrund,grundMatrix):
+  newGrundTrustMatrix = []
+  saveIndex = []
+  if (len(fileNameText) != len(fileNameGrund)):
+    for i in range (len(fileNameText)):
+      cuttedFileName = os.path.basename(fileNameText[i]).split('.',1)[0]
+      for j in range(0,len(fileNameGrund)):
+        cuttedFileNameGTM =(fileNameGrund[j].split('/'))[4].split('.',1)[0]
+        if (cuttedFileName == cuttedFileNameGTM):
+          saveIndex.append(j)
+    for k in range(0,len(saveIndex)):
+      row = grundMatrix[saveIndex[k]]
+      newrow = []
+      for l in range(0,len(saveIndex)):
+        newrow.append(row[saveIndex[l]])
+      newGrundTrustMatrix.append(newrow)
+
+    return numpy.array(newGrundTrustMatrix)
+  else:
+    return grundMatrix
